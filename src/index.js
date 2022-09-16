cd8950b437593e965c0ea8a88c06084e7a9a9d03
yuichiro-otomo <112527904+yuichiro-otomo@users.noreply.github.com>
2022-09-16 17:33:00 +0900

    練習let

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import "./styles.css";
+/**
+ * const letの変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!あ</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数は上書き可能";
+
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+//letは上書き可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+//var変数は再宣言不可
+//var val2 = "var変数を再宣言";
